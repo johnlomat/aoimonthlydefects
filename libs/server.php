b33d34b193be1bf9f982ae3a4647b5696600fae5
johnlomat <54453015+johnlomat@users.noreply.github.com>
2020-09-10 23:40:30 +0800

    add remote sql

--- libs/server.php
+++ libs/server.php
@@ -1,4 +1,8 @@
 <?php
-	$connect = new PDO("mysql:host=localhost;dbname=aoi_monthly_defects","root","root");
+	// Development Connection
+	// $connect = new PDO("mysql:host=localhost;dbname=aoi_monthly_defects","root","root");
+
+	// Remote SQL Connection
+	$connect = new PDO("mysql:host=remotemysql.com;dbname=OuqhQAwYnh","OuqhQAwYnh","7DjFAmL7Ey");
 	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
