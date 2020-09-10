e3999e2b278229c898d13e25825a7e4f5731aa1d
johnlomat <54453015+johnlomat@users.noreply.github.com>
2020-09-10 23:45:52 +0800

    added remote sql connection

--- libs/db_registration.php
+++ libs/db_registration.php
@@ -13,7 +13,11 @@
 	
 	if(isset($_POST['reg_user'])) {
 		try {
-			$connect = new PDO("mysql:host=localhost;dbname=aoi_monthly_defects","root","root");
+			// Development Connection
+			// $connect = new PDO("mysql:host=localhost;dbname=aoi_monthly_defects","root","root");
+
+			// Remote SQL Connection
+			$connect = new PDO("mysql:host=remotemysql.com;dbname=OuqhQAwYnh","OuqhQAwYnh","7DjFAmL7Ey");
 			$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$password = $password_1;
 			
